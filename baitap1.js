922bd3a27d07b11b191d5ad7c4e40343b480de30
Uyen <tav2512@gmail.com>
2022-10-09 20:29:27 +0700

    nopbai

---
+++ baitap1.js
@@ -0,0 +1,29 @@
+/** Mô hình 3 khối (viết chương trình tính lương nhân viên)
+ * 
+ * Khối 1: input(dữ liệu được cung cấp)
+ *      số ngày làm: 27
+ *      lương 1 ngày: 100000
+ * ?luong1ngay ,songaylam
+ * Khối 2: các bước xử lí
+ *  lập công thức tính toán
+ *      lương = lương 1 ngày * số ngày làm
+ *  thông báo kết quả
+ * Khối 3: output tiền lương nhận được
+ * ?luongnhanduoc
+ */
+
+
+//? b1
+var luong1ngay = 100000;
+var songaylam = 27;
+
+
+//? b2
+luongnhanduoc = luong1ngay * songaylam;
+
+//?
+console.log("Lương nhận được là" + luongnhanduoc + "đồng");
+
+
+
+
---
+++ baitap2.js
@@ -0,0 +1,4 @@
+/** Mô hình 3 khối (tính giá trị trung bình 5 số)
+ * b1: nhập 5 số 
+ * b2
+ */
