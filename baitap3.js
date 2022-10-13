066eead113599f1222e49e632d461ec57e6621c9
uyen2512 <115127907+uyen2512@users.noreply.github.com>
2022-10-13 11:41:18 +0700

    Update baitap3.js

--- baitap3.js
+++ baitap3.js
@@ -1,16 +1,18 @@
-/**(mô hình 3 khối quy đổi tiền $)
- * b1: nhập vào số $ cần đổi và tỉ giá hiện tại
- * b2: thực hiện phép tính quy đổi: số tiền * tỉ giá hiện tại
+/** mô hình 3 khối 
+ * b1: nhập chiều dài chiều rộng của hcn
+ * b2: thực hiện phép tính: chu vi = (d+r)*2 ! diện tích = d*r 
  * b3: thông báo kết quả
  */
 
-//? b1: 
-var tiGia = 23500;
-var soTien$ = 2;
-var soTienVND = 0;
+//? b1:
+var cdai = 14;
+var crong = 10;
+var cvi = 0;
+var dtich = 0;
 
-//? b2: 
-soTienVND = soTien$ * tiGia
+//? b2
+cvi = (cdai + crong) * 2;
+dtich = cdai * crong;
 
-//? b3
-console.log("So tien VND tương ứng là: " + soTienVND + " đồng"); 
+//? b3 
+console.log(`Chu vi hình chữ nhật là: ${cvi} \nDiện tích hình chữ nhật là: ${dtich}`)
